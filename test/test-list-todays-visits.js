439ad1a6999de3e79ed4eed8fd60b23bfa42093a
hangilc <hangil@chang.jp>
2016-06-26 16:15:28 +0900

    revised test for list todays visits

--- test/test-list-todays-visits.js
+++ test/test-list-todays-visits.js
@@ -19,6 +19,80 @@ function createMockPatients(n){
 	});
 }
 
+var retry = 1;
+
+function testSimple(conn, done){
+	var patients = createMockPatients(20);
+	var items = [];
+	var nPrev = 4;
+	var startDate = moment().format("YYYY-MM-DD");
+	conti.exec([
+		function(done){
+			util.batchInsertPatients(conn, patients, done);
+		},
+		function(done){
+			patients.forEach(function(patient, i){
+				var n = 1;
+				if( i === 3 || i === 10 ){
+					n = 2;
+				} else if( i === 6 ){
+					n = 3;
+				}
+				for(var j=0; j<n; j++){
+					items.push({
+						patient: patient,
+						visit: util.mockVisit({patient_id: patient.patient_id,
+							v_datetime: moment().format("YYYY-MM-DD HH:mm:ss")})
+					})
+				}
+			});
+			items.slice(0, nPrev).forEach(function(item){
+				item.visit.v_datetime = moment().add(-1, "day").format("YYYY-MM-DD HH:mm:ss");
+			})
+			var visits = items.map(function(item){ return item.visit; });
+			util.batchInsertVisits(conn, visits, done);
+		}
+	], function(err){
+		if( err ){
+			done(err);
+			return;
+		}
+		var ans = items.slice(nPrev).map(function(item){
+			var visit = item.visit;
+			var patient = item.patient;
+			return {
+				visit_id: visit.visit_id,
+				patient_id: patient.patient_id,
+				last_name: patient.last_name,
+				first_name: patient.first_name,
+				last_name_yomi: patient.last_name_yomi,
+				first_name_yomi: patient.first_name_yomi
+			}
+		})
+		db.listTodaysVisits(conn, function(err, result){
+			if( err ){
+				done(err);
+				return;
+			}
+			var endDate = moment().format("YYYY-MM-DD");
+			if( startDate !== endDate ){
+				if( retry-- > 0 ){
+					util.resetTables(conn, ["patient", "visit"], function(err){
+						if( err ){
+							done(err);
+							return;
+						}
+						testSimple(conn, done);
+					});
+					return;
+				}
+			}
+			expect(result).eql(ans);
+			done();
+		})
+	})
+}
+
 describe("Testing list todays visits", function(){
 	var conn;
 
@@ -49,60 +123,6 @@ describe("Testing list todays visits", function(){
 	});
 
 	it("simple", function(done){
-		var patients = createMockPatients(20);
-		var items = [];
-		var nPrev = 4;
-		conti.exec([
-			function(done){
-				util.batchInsertPatients(conn, patients, done);
-			},
-			function(done){
-				patients.forEach(function(patient, i){
-					var n = 1;
-					if( i === 3 || i === 10 ){
-						n = 2;
-					} else if( i === 6 ){
-						n = 3;
-					}
-					for(var j=0; j<n; j++){
-						items.push({
-							patient: patient,
-							visit: util.mockVisit({patient_id: patient.patient_id,
-								v_datetime: moment().format("YYYY-MM-DD HH:mm:ss")})
-						})
-					}
-				});
-				items.slice(0, nPrev).forEach(function(item){
-					item.visit.v_datetime = moment().add(-1, "day").format("YYYY-MM-DD HH:mm:ss");
-				})
-				var visits = items.map(function(item){ return item.visit; });
-				util.batchInsertVisits(conn, visits, done);
-			}
-		], function(err){
-			if( err ){
-				done(err);
-				return;
-			}
-			var ans = items.slice(nPrev).map(function(item){
-				var visit = item.visit;
-				var patient = item.patient;
-				return {
-					visit_id: visit.visit_id,
-					patient_id: patient.patient_id,
-					last_name: patient.last_name,
-					first_name: patient.first_name,
-					last_name_yomi: patient.last_name_yomi,
-					first_name_yomi: patient.first_name_yomi
-				}
-			})
-			db.listTodaysVisits(conn, function(err, result){
-				if( err ){
-					done(err);
-					return;
-				}
-				expect(result).eql(ans);
-				done();
-			})
-		})
+		testSimple(conn, done);
 	});
 })

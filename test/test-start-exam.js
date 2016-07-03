0666732519616f6189898e797e13575e3bb35c88
hangilc <hangil@chang.jp>
2016-07-03 12:03:12 +0900

    implemented suspendExam in start-visit.js

--- test/test-start-exam.js
+++ test/test-start-exam.js
@@ -18,7 +18,7 @@ function initDb(done){
 	}, done);
 }
 
-describe("Testing new test", function(){
+describe("Testing startExam", function(){
 	var conn;
 
 	beforeEach(initDb);
@@ -112,4 +112,96 @@ describe("Testing new test", function(){
 		], done);
 	});
 
-})
+});
+
+describe("Testing suspendExam", function(done){
+	var conn;
+
+	beforeEach(initDb);
+
+	beforeEach(function(done){
+		setup.connect(function(err, conn_){
+			if( err ){
+				done(err);
+				return;
+			}
+			conn = conn_;
+			done();
+		})
+	});
+
+	afterEach(function(done){
+		setup.release(conn, done);
+	});
+
+	afterEach(initDb);
+
+	var valid_from = "2016-04-01";
+	var at = "2016-06-26 21:35:21";
+	var valid_upto = "2018-03-31";
+	var valid_upto_no_limit = "0000-00-00";
+
+	it("minimal", function(done){
+		var visitId = 1000;
+		var wq = m.wqueue({visit_id: visitId, wait_state: uConst.wqueueStateInExam});
+		conti.exec([
+			function(done){
+				wq.save(conn, done);
+			},
+			function(done){
+				db.suspendExam(conn, visitId, done);
+			},
+			function(done){
+				db.getWqueue(conn, visitId, function(err, result){
+					if( err ){
+						done(err);
+						return;
+					}
+					var ans = {
+						visit_id: visitId,
+						wait_state: uConst.wqueueStateWaitReExam
+					}
+					expect(result).eql(ans);
+					done();
+				})
+			}
+		], done);
+	});
+
+	it("with startVisit/startExam", function(done){
+		var patientId = 1200;
+		var visitId;
+		conti.exec([
+			function(done){
+				db.startVisit(conn, patientId, at, function(err, visitId_){
+					if( err ){
+						done(err);
+						return;
+					}
+					visitId = visitId_;
+					done();
+				});
+			},
+			function(done){
+				db.startExam(conn, visitId, done);
+			},
+			function(done){
+				db.suspendExam(conn, visitId, done);
+			},
+			function(done){
+				db.getWqueue(conn, visitId, function(err, result){
+					if( err ){
+						done(err);
+						return;
+					}
+					var ans = {
+						visit_id: visitId,
+						wait_state: uConst.wqueueStateWaitReExam
+					}
+					expect(result).eql(ans);
+					done();
+				})
+			}
+		], done);
+	});
+});

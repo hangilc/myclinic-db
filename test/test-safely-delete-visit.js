dfb461a9c7778eedea7b2ff036ce11b5e4eb0c46
hangilc <hangil@chang.jp>
2016-07-04 23:11:32 +0900

    implemented safelyDeleteVisit

--- test/test-safely-delete-visit.js
+++ test/test-safely-delete-visit.js
@@ -44,7 +44,7 @@ describe("Testing safely delete visit", function(){
 	var valid_upto = "2018-03-31";
 	var valid_upto_no_limit = "0000-00-00";
 
-	it("empty", function(done){
+	it("minimal", function(done){
 		var visit = m.visit();
 		var visitId;
 		conti.exec([
@@ -54,6 +54,16 @@ describe("Testing safely delete visit", function(){
 			function(done){
 				visitId = visit.data.visit_id;
 				db.safelyDeleteVisit(conn, visitId, done);
+			},
+			function(done){
+				db.findVisit(conn, visitId, function(err, result){
+					if( err ){
+						done(err);
+						return;
+					}
+					expect(result).null;
+					done();
+				})
 			}
 		], done);
 	});
@@ -83,6 +93,123 @@ describe("Testing safely delete visit", function(){
 					expect(result).null;
 					done();
 				})
+			}
+		], done);
+	});
+
+	it("confirm wqueue is also deleted", function(done){
+		var visit = m.visit();
+		var visitId;
+		var wq;
+		conti.exec([
+			function(done){
+				visit.save(conn, done);
+			},
+			function(done){
+				visitId = visit.data.visit_id;
+				wq = m.wqueue({visit_id: visitId});
+				wq.save(conn, done);
+			},
+			function(done){
+				db.safelyDeleteVisit(conn, visitId, done);
+			},
+			function(done){
+				db.findWqueue(conn, visitId, function(err, result){
+					if( err ){
+						done(err);
+						return;
+					}
+					expect(result).null;
+					done();
+				})
+			}
+		], done);
+	});
+
+	it("confirm fail if text exists", function(done){
+		var visit = m.visit();
+		var visitId;
+		var text;
+		conti.exec([
+			function(done){
+				visit.save(conn, done);
+			},
+			function(done){
+				visitId = visit.data.visit_id;
+				text = m.text({visit_id: visitId});
+				text.save(conn, done);
+			},
+			function(done){
+				db.safelyDeleteVisit(conn, visitId, function(err){
+					expect(err).ok;
+					done();
+				});
+			}
+		], done);
+	})
+
+	it("confirm fail if drug exists", function(done){
+		var visit = m.visit();
+		var visitId;
+		var drug;
+		conti.exec([
+			function(done){
+				visit.save(conn, done);
+			},
+			function(done){
+				visitId = visit.data.visit_id;
+				drug = m.drug({visit_id: visitId});
+				drug.save(conn, done);
+			},
+			function(done){
+				db.safelyDeleteVisit(conn, visitId, function(err){
+					expect(err).ok;
+					done();
+				});
+			}
+		], done);
+	})
+
+	it("confirm fail if shinryou exists", function(done){
+		var visit = m.visit();
+		var visitId;
+		var shinryou;
+		conti.exec([
+			function(done){
+				visit.save(conn, done);
+			},
+			function(done){
+				visitId = visit.data.visit_id;
+				shinryou = m.shinryou({visit_id: visitId});
+				shinryou.save(conn, done);
+			},
+			function(done){
+				db.safelyDeleteVisit(conn, visitId, function(err){
+					expect(err).ok;
+					done();
+				});
+			}
+		], done);
+	})
+
+	it("confirm fail if conduct exists", function(done){
+		var visit = m.visit();
+		var visitId;
+		var conduct;
+		conti.exec([
+			function(done){
+				visit.save(conn, done);
+			},
+			function(done){
+				visitId = visit.data.visit_id;
+				conduct = m.conduct({visit_id: visitId});
+				conduct.save(conn, done);
+			},
+			function(done){
+				db.safelyDeleteVisit(conn, visitId, function(err){
+					expect(err).ok;
+					done();
+				});
 			}
 		], done);
 	})

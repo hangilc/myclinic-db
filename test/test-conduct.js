2400e2e7120c062e6de4a9cf9737c8e1fb5d904b
Hangil Chang <hangil@chang.jp>
2016-07-04 17:21:03 +0900

    implemented countConductsForVisit

--- test/test-conduct.js
+++ test/test-conduct.js
@@ -2,6 +2,8 @@ var setup = require("./setup");
 var expect = require("chai").expect;
 var util = require("./util");
 var db = require("../index");
+var conti = require("../lib/conti");
+var m = require("./model");
 
 var clearTable = util.createClearTableFun("visit_conduct");
 
@@ -117,3 +119,95 @@ describe("Testing conduct", function(){
 		})
 	})
 });
+
+describe("Testing count conducts", function(){
+	var conn;
+
+	beforeEach(clearTable);
+	beforeEach(function(done){
+		setup.connect(function(err, conn_){
+			if( err ){
+				done(err);
+				return;
+			}
+			conn = conn_;
+			done();
+		})
+	});
+
+	afterEach(function(done){
+		setup.release(conn, done);
+	});
+	afterEach(clearTable);
+
+	it("empty", function(done){
+		db.countConductsForVisit(conn, 0, function(err, result){
+			if( err ){
+				done(err);
+				return;
+			}
+			expect(result).equal(0);
+			done();
+		})
+	});
+
+	it("simple", function(done){
+		var visitId = 8888;
+		var n = 3;
+		var conducts = util.iterMap(n, function(i){
+			return m.conduct({
+				visit_id: visitId
+			});
+		});
+		conti.exec([
+			function(done){
+				m.batchSave(conn, conducts, done);
+			},
+			function(done){
+				db.countConductsForVisit(conn, visitId, function(err, result){
+					if( err ){
+						done(err);
+						return;
+					}
+					expect(result).equal(n);
+					done();
+				})
+			}
+		], done);
+	})
+
+	it("multiple visits", function(done){
+		var visitId = 7777;
+		var visitId2 = 6666;
+		var n = 3;
+		var conducts = util.iterMap(n, function(i){
+			return m.conduct({
+				visit_id: visitId
+			});
+		});
+		var conducts2 = util.iterMap(n, function(i){
+			return m.conduct({
+				visit_id: visitId2
+			});
+		});
+		conti.exec([
+			function(done){
+				m.batchSave(conn, conducts, done);
+			},
+			function(done){
+				m.batchSave(conn, conducts2, done);
+			},
+			function(done){
+				db.countConductsForVisit(conn, visitId, function(err, result){
+					if( err ){
+						done(err);
+						return;
+					}
+					expect(result).equal(n);
+					done();
+				})
+			}
+		], done);
+	})
+
+})

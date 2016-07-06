7c2adfe371da8b567dc5c437d671f854ae3e7c9e
hangilc <hangil@chang.jp>
2016-07-06 11:24:12 +0900

    implemented searchShoubyoumeiMaster

---
+++ test/test-search-shoubyoumei-master.js
@@ -0,0 +1,128 @@
+"use strict";
+
+var setup = require("./setup");
+var db = require("../index");
+var expect = require("chai").expect;
+var DbUtil = require("./db-util");
+var util = require("./util");
+var conti = require("../lib/conti");
+var m = require("./model");
+
+var initDb = util.createClearTableFun(["shoubyoumei_master_arch"]);
+
+describe("Testing shoubyoumei master", function(){
+	var conn = setup.getConnection();
+
+	beforeEach(initDb);
+	afterEach(initDb);
+
+	var valid_from = "2016-04-01";
+	var at = "2016-06-26 21:35:21";
+	var valid_upto = "2018-03-31";
+	var valid_upto_no_limit = "0000-00-00";
+
+	it("none", function(done){
+		db.searchShoubyoumeiMaster(conn, "テスト", at, function(err, result){
+			if( err ){
+				done(err);
+				return;
+			}
+			expect(result).eql([]);
+			done();
+		});
+	});
+
+	function addMasters(names, done){
+		conti.forEach(names, function(name, done){
+			var master = m.shoubyoumeiMaster({
+				valid_from: valid_from,
+				valid_upto: valid_upto,
+				name: name
+			});
+			master.save(conn, done);
+		}, done);
+	}
+
+	it("case 1", function(done){
+		conti.exec([
+			function(done){
+				addMasters(["編症似", "亜症像"], done);
+			},
+			function(done){
+				db.searchShoubyoumeiMaster(conn, "編症似", at, function(err, result){
+					if( err ){
+						done(err);
+						return;
+					}
+					expect(util.pluck(result, "name")).eql(["編症似"]);
+					done();
+				})
+			}
+		], done);
+	});
+
+	it("case 2", function(done){
+		conti.exec([
+			function(done){
+				addMasters(["編症似", "亜症像"], done);
+			},
+			function(done){
+				db.searchShoubyoumeiMaster(conn, "症", at, function(err, result){
+					if( err ){
+						done(err);
+						return;
+					}
+					expect(util.pluck(result, "name")).eql(["亜症像", "編症似"]);
+					done();
+				})
+			}
+		], done);
+	});
+
+	it("case 3", function(done){
+		conti.exec([
+			function(done){
+				addMasters(["編症似", "亜症像"], done);
+			},
+			function(done){
+				db.searchShoubyoumeiMaster(conn, "痛", at, function(err, result){
+					if( err ){
+						done(err);
+						return;
+					}
+					expect(util.pluck(result, "name")).eql([]);
+					done();
+				})
+			}
+		], done);
+	});
+
+	it("case 4", function(done){
+		var master1 = m.shoubyoumeiMaster({
+			name: "編症似",
+			valid_from: util.incDay(at, -100),
+			valid_upto: util.incDay(at, -10)
+		});
+		var master2 = m.shoubyoumeiMaster({
+			name: "亜症像",
+			valid_from: valid_from,
+			valid_upto: valid_upto
+		});
+		conti.exec([
+			function(done){
+				m.batchSave(conn, [master1, master2], done);
+			},
+			function(done){
+				db.searchShoubyoumeiMaster(conn, "症", at, function(err, result){
+					if( err ){
+						done(err);
+						return;
+					}
+					expect(util.pluck(result, "name")).eql(["亜症像"]);
+					done();
+				})
+			}
+		], done);
+	});
+
+})

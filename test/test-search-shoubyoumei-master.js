76c95ab8ff63311db49b0b1f8e30cbfa36f25b3f
hangilc <hangil@chang.jp>
2016-07-06 12:01:30 +0900

    added getShoubyoumeiMasterByName

--- test/test-search-shoubyoumei-master.js
+++ test/test-search-shoubyoumei-master.js
@@ -125,4 +125,81 @@ describe("Testing shoubyoumei master", function(){
 		], done);
 	});
 
-})
+});
+
+describe("Testing get shoubyoumei by name", function(){
+	var conn = setup.getConnection();
+
+	beforeEach(initDb);
+	afterEach(initDb);
+
+	var valid_from = "2016-04-01";
+	var at = "2016-06-26 21:35:21";
+	var valid_upto = "2018-03-31";
+	var valid_upto_no_limit = "0000-00-00";
+
+	it("fail", function(done){
+		db.getShoubyoumeiMasterByName(conn, "あいう", at, function(err){
+			expect(err).ok;
+			done();
+		})
+	});
+
+	function addMasters(names, done){
+		conti.forEach(names, function(name, done){
+			var master = m.shoubyoumeiMaster({
+				valid_from: valid_from,
+				valid_upto: valid_upto,
+				name: name
+			});
+			master.save(conn, done);
+		}, done);
+	}
+
+	it("case 1", function(done){
+		conti.exec([
+			function(done){
+				addMasters(["あいう", "かきく", "さしす"], done);
+			},
+			function(done){
+				db.getShoubyoumeiMasterByName(conn, "かきく", at, function(err, result){
+					if( err ){
+						done(err);
+						return;
+					}
+					expect(result.name).eql("かきく");
+					done();
+				})
+			}
+		], done);
+	});
+
+	it("case 2", function(done){
+		var master1 = m.shoubyoumeiMaster({
+			name: "あいう",
+			valid_from: util.incDay(at, -100),
+			valid_upto: util.incDay(at, -10)
+		});
+		var master2 = m.shoubyoumeiMaster({
+			name: "あいう",
+			valid_from: valid_from,
+			valid_upto: valid_upto
+		});
+		conti.exec([
+			function(done){
+				m.batchSave(conn, [master1, master2], done);
+			},
+			function(done){
+				db.getShoubyoumeiMasterByName(conn, "あいう", at, function(err, result){
+					if( err ){
+						done(err);
+						return;
+					}
+					expect(result.name).eql("あいう");
+					done();
+				})
+			}
+		], done);
+	});
+
+});

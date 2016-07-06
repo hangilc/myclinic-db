363277ca0779d9cbecf68ef163f451ecff8fdb7a
hangilc <hangil@chang.jp>
2016-07-06 13:43:32 +0900

    implemented getShuushokugoMasterByName

---
+++ test/test-list-disease-adj-for-disease.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var setup = require("./setup");
+var db = require("../index");
+var expect = require("chai").expect;
+var DbUtil = require("./db-util");
+var util = require("./util");
+var conti = require("../lib/conti");
+var m = require("./model");
+
+var initDb = util.createClearTableFun(["visit_text"]);
+
+describe("Testing new test", function(){
+	var conn = setup.getConnection();
+
+	beforeEach(initDb);
+	afterEach(initDb);
+
+	var valid_from = "2016-04-01";
+	var at = "2016-06-26 21:35:21";
+	var valid_upto = "2018-03-31";
+	var valid_upto_no_limit = "0000-00-00";
+
+	it("mock", function(done){
+		done();
+	});
+
+})
--- test/test-search-shuushokugo-master.js
+++ test/test-search-shuushokugo-master.js
@@ -95,32 +95,52 @@ describe("Testing search shuushokugo master", function(){
 		], done);
 	});
 
-	it("case 4", function(done){
-		var master1 = m.shoubyoumeiMaster({
-			name: "あいう",
-			valid_from: util.incDay(at, -100),
-			valid_upto: util.incDay(at, -10)
-		});
-		var master2 = m.shoubyoumeiMaster({
-			name: "おいえ",
-			valid_from: valid_from,
-			valid_upto: valid_upto
-		});
+});
+
+describe("Testing get shuushokugo by name", function(){
+	var conn = setup.getConnection();
+
+	beforeEach(initDb);
+	afterEach(initDb);
+
+	var valid_from = "2016-04-01";
+	var at = "2016-06-26 21:35:21";
+	var valid_upto = "2018-03-31";
+	var valid_upto_no_limit = "0000-00-00";
+
+	it("fail", function(done){
+		db.getShuushokugoMasterByName(conn, "あいう", function(err){
+			expect(err).ok;
+			done();
+		})
+	});
+
+	function addMasters(names, done){
+		conti.forEach(names, function(name, done){
+			var master = m.shuushokugoMaster({
+				name: name
+			});
+			master.save(conn, done);
+		}, done);
+	}
+
+	it("case 1", function(done){
 		conti.exec([
 			function(done){
-				m.batchSave(conn, [master1, master2], done);
+				addMasters(["あいう", "かきく", "さしす"], done);
 			},
 			function(done){
-				db.searchShoubyoumeiMaster(conn, "い", at, function(err, result){
+				db.getShuushokugoMasterByName(conn, "かきく", function(err, result){
 					if( err ){
 						done(err);
 						return;
 					}
-					expect(util.pluck(result, "name")).eql(["おいえ"]);
+					expect(result.name).eql("かきく");
 					done();
 				})
 			}
 		], done);
 	});
 
-})
+});
+

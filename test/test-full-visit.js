e83c255b5af1e3c79a3349d25c6bf3dfaa45a0d7
hangilc <hangil@chang.jp>
2016-07-24 23:54:05 +0900

    implemented getVisitWithFullHoken

--- test/test-full-visit.js
+++ test/test-full-visit.js
@@ -153,4 +153,119 @@ describe("Testing full visit", function(){
 			})
 		})
 	})
+});
+
+describe("Testing full hoken", function(){
+	var conn = setup.getConnection();
+
+	beforeEach(function(done){
+		resetTables(conn, done);
+	});
+
+	afterEach(function(done){
+		this.timeout(10000);
+		resetTables(conn, done);
+	})
+
+	var valid_from = "2016-04-01";
+	var at = "2016-06-26 21:35:21";
+	var valid_upto = "0000-00-00";
+
+	it("empty", function(done){
+		var visit = m.visit({
+			shahokokuho_id: 0,
+			koukikourei_id: 0,
+			roujin_id: 0,
+			kouhi_1_id: 0,
+			kouhi_2_id: 0,
+			kouhi_3_id: 0
+		});
+		conti.exec([
+			function(done){
+				visit.save(conn, done);
+			},
+			function(done){
+				db.getVisitWithFullHoken(conn, visit.data.visit_id, function(err, result){
+					if( err ){
+						done(err);
+						return;
+					}
+					expect(result.shahokokuho).null;
+					expect(result.koukikourei).null;
+					expect(result.roujin).null;
+					expect(result.kouhi_list).eql([]);
+					done();
+				})
+			}
+		], done)
+	});
+
+	it("full", function(done){
+		var patientId = 2222;
+		var visit = {
+			patient_id: patientId,
+			v_datetime: at,
+			shahokokuho_id: 0,
+			koukikourei_id: 0,
+			roujin_id: 0,
+			kouhi_1_id: 0,
+			kouhi_2_id: 0,
+			kouhi_3_id: 0
+		};
+		var shahokokuho = m.shahokokuho({
+			patient_id: patientId,
+			valid_from: valid_from,
+			valid_upto: valid_upto
+		});
+		var koukikourei = m.koukikourei({
+			patient_id: patientId,
+			valid_from: valid_from,
+			valid_upto: valid_upto
+		});
+		var roujin = m.roujin({
+			patient_id: patientId,
+			valid_from: valid_from,
+			valid_upto: valid_upto
+		});
+		var kouhi_1 = m.kouhi({
+			patient_id: patientId,
+			valid_from: valid_from,
+			valid_upto: valid_upto
+		});
+		var kouhi_2 = m.kouhi({
+			patient_id: patientId,
+			valid_from: valid_from,
+			valid_upto: valid_upto
+		});
+		var kouhi_3 = m.kouhi({
+			patient_id: patientId,
+			valid_from: valid_from,
+			valid_upto: valid_upto
+		});
+		conti.exec([
+			function(done){
+				m.batchSave(conn, [shahokokuho, koukikourei, roujin, kouhi_1, kouhi_2, kouhi_3], done);
+			},
+			function(done){
+				util.assign(visit, {
+					shahokokuho_id: shahokokuho.data.shahokokuho_id,
+					koukikourei_id: koukikourei.data.koukikourei_id,
+					roujin_id: roujin.data.roujin_id,
+					kouhi_1_id: kouhi_1.data.kouhi_id,
+					kouhi_2_id: kouhi_2.data.kouhi_id,
+					kouhi_3_id: kouhi_3.data.kouhi_id
+				});
+				db.insertVisit(conn, visit, done);
+			},
+			function(done){
+				db.getVisitWithFullHoken(conn, visit.visit_id, function(err, result){
+					expect(result.shahokokuho).eql(shahokokuho.data);
+					expect(result.koukikourei).eql(koukikourei.data);
+					expect(result.roujin).eql(roujin.data);
+					expect(result.kouhi_list).eql([kouhi_1.data, kouhi_2.data, kouhi_3.data]);
+					done();
+				})
+			}
+		], done);
+	})
 })
